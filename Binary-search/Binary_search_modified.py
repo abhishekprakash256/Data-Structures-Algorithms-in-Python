3ed0d23068d0c5d1a571d895fefb3d3a8561c2f7
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-09-12 20:12:52 +0000

    modifeid search failed

--- Binary-search/Binary_search_modified.py
+++ Binary-search/Binary_search_modified.py
@@ -7,6 +7,31 @@ from Binary_search import *
 
 
 class Modify_Sol(Solution):
+    def return_val(self,lst,left,right):
+        """
+        The function to check for duplicate value and return val
+        
+        Args:
+            lst (list): The list of the numbers 
+            left: (int): The pointer index value
+            right: (int): The pointer index value 
+        
+        Return:
+            index (int): The index value of the query  
+        """
+
+        while left <= right:
+
+            mid = (left + right) // 2 
+
+            if (lst[mid] == query and lst[mid - 1] != query) or (lst[mid] == query and lst[mid] == 0  ):
+                return mid
+
+            else:
+                right = mid - 1  
+
+
+
     def modified_binary_search(self,lst,query)-> int:
         """
         The function to find the value in the list even repeated elements 
@@ -18,4 +43,25 @@ class Modify_Sol(Solution):
             index (int) : The index value of the query 
         """
 
-        
+        left = 0 
+        right = len(lst) - 1 
+
+        while left <= right:
+
+            mid = (left+right) // 2          
+
+            if lst[mid] == query and mid !=0 and lst[mid-1] == query:
+                self.return_val(lst,left,right)
+
+            elif lst[mid] == query and lst[mid - 1] != query:
+                return mid  
+            
+            elif lst[mid] < query:
+
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return -1
+
