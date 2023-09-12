afa21c8d2a0c0bc239c6f01f8d3e79b858840f62
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-09-12 19:36:03 +0000

    modified search

--- Binary-search/Binary_search_modified.py
+++ Binary-search/Binary_search_modified.py
@@ -5,3 +5,17 @@ for the repetative element in the list
 
 from Binary_search import * 
 
+
+class Modify_Sol(Solution):
+    def modified_binary_search(self,lst,query)-> int:
+        """
+        The function to find the value in the list even repeated elements 
+        Args:
+            lst (list) : The sorted list of integers 
+            query (int) : The query to find in the list 
+
+        Returns:
+            index (int) : The index value of the query 
+        """
+
+        
