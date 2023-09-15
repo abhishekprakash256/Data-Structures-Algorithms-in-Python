1d91f5e702ce7f5724d984dd5c492b234a51448a
abhishekprakash256 <abhisheprakash47@gmail.com>
2023-09-15 20:17:14 +0000

    corect code for binary search

--- Binary-search/Binary_search_modified.py
+++ Binary-search/Binary_search_modified.py
@@ -2,6 +2,7 @@
 The implementation of the modified binary search 
 for the repetative element in the list
 """
+
 #----------------------------------the working code for the problem---------------------------------------------#
 
 
