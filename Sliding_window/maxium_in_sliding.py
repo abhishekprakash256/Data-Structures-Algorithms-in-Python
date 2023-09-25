fd0e48cd3725a190ad342084fd6bdbe54f891dfd
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-09-24 23:29:50 -0400

    sliding widnow

---
+++ Sliding_window/maxium_in_sliding.py
@@ -0,0 +1,26 @@
+"""
+Given an integer list, nums, find the maximum values in all the contiguous subarrays (windows) of size w.
+
+"""
+
+
+#for this question we need to make a deccreasing monotonic queue 
+
+
+#test cases
+
+lst1 = [4,5,2,4,4,6,1,7,8,0]
+
+
+lst2 = [8,6,4,7,0,1,4,3,6,4]
+
+
+
+class Solution:
+	def decrease_queue(self,lst):
+		"""
+		The function to make the decreasing queue
+
+
+		"""
+		pass
